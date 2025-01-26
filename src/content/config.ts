a48d881d22e297dd02c30e53db74408f15657f31
jeronimomayorca <mayorcajeronimo@gmail.com>
2025-01-25 22:53:37 -0500

    fix problems of migrating to astro v5.1

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
 import { z, defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
 
 const servicesCollection = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/content/services" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
